ed8a27da7a334ebc0a9a3c5f4f83837f078cbef3
Arshnoor03 <aboparai18@my.bcit.ca>
2023-11-05 21:17:35 -0800

    added sql file

---
+++ server1/characterDict.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 05, 2023 at 05:42 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `characterDict`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dictionary`
+--
+
+CREATE TABLE `dictionary` (
+  `id` int(11) NOT NULL,
+  `word` varchar(255) NOT NULL,
+  `definition` text DEFAULT NULL,
+  `word_language` varchar(255) NOT NULL,
+  `definition_language` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `language`
+--
+
+CREATE TABLE `language` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `language`
+--
+
+INSERT INTO `language` (`id`, `name`) VALUES
+(1, 'English'),
+(2, 'Spanish'),
+(4, 'Spanish'),
+(5, 'French'),
+(6, 'German'),
+(7, 'Chinese (Simplified)'),
+(8, 'Chinese (Traditional)'),
+(9, 'Arabic'),
+(10, 'Russian'),
+(11, 'Portuguese'),
+(12, 'Japanese');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dictionary`
+--
+ALTER TABLE `dictionary`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `language`
+--
+ALTER TABLE `language`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dictionary`
+--
+ALTER TABLE `dictionary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `language`
+--
+ALTER TABLE `language`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
